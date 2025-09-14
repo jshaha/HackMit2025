a21070069f82a35a43fd31efb33b7f3932ece8f9
jshaha <shahatitjacob@gmail.com>
2025-09-13 23:45:12 -0400

    Revert "added auth"
    This reverts commit 39aa432cb3b7b52a856f3af4d94c42e85d17f556.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Auth0Provider } from '@auth0/auth0-react';
 import App from "./App";
 import "./index.css";
-import { env } from "./env";
 
-createRoot(document.getElementById("root")!).render(
-  <Auth0Provider
-    domain={env.AUTH0_DOMAIN}
-    clientId={env.AUTH0_CLIENT_ID}
-    authorizationParams={{
-      redirectUri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
